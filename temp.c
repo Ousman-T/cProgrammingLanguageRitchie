ccef24af3d6f0effe9dfbf183abb7cdf1dbfd163
Ousman Touray <otouray14@gmail.com>
2025-02-03 00:31:04 -0500

    reverse list

--- temp.c
+++ temp.c
@@ -3,7 +3,7 @@
     int main() {
         int fahr;
         printf("Fahrenheit Celcius\n");
-        for (fahr = 0; fahr <= 300; fahr += 20){
+        for (fahr = 300; fahr >= 0; fahr -= 20){
             printf("%4d %6.1f\n", fahr, (5.0/9.0) *(fahr-32));
         } 
         return 0;
