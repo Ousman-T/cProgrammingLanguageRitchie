d3c62786dcf31d6b8deb3ee9288b418208424753
Ousman Touray <otouray14@gmail.com>
2025-02-04 00:33:15 -0500

    last numCount

--- numCount.c
+++ numCount.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
     int main() {
-        long nc = 0;
-        while (getchar() != EOF) {
-            nc++;
-            printf("%1d\n", nc);
+        double nc;
+        for (nc = 0; getchar() != EOF; nc++) {
+            printf("%.f\n", nc);
         }
-        return 0;
     }
